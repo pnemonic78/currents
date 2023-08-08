c0baf8f772bda6192fe04f82797e00befdf67321
M. Waisberg <pnemonic@gmail.com>
2023-08-08 11:39:55 +0300

    splash screen

---
+++ app/lib/my/my_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'my_home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Currents API',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Currents API Home Page'),
+    );
+  }
+}
+
